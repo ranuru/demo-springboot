283c40e9e57b589c2311fffa3b1ebab8a4d44cec
Runar Røssevold <runarrossevold@gmail.com>
2025-10-03 11:47:27 +0000

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
     implementation("org.hibernate.orm:hibernate-core:7.1.1.Final")
     implementation("jakarta.persistence:jakarta.persistence-api:3.2.0")
     implementation("com.h2database:h2:2.3.232")
+    implementation("redis.clients:jedis:6.2.0")
 }
 
 tasks.withType<Test> {
